7fd1872f6aa7da7ebb7a8ad4aa96b636fc1d26f5
oualid kiram <oualid.kiram1@gmail.com>
2024-09-18 13:49:00 -0400

    refactor : Rectangle game

---
+++ src/MovingRectangle/App.java
@@ -0,0 +1,12 @@
+package MovingRectangle;
+
+import Doctrina.Game;
+
+public class App {
+
+    public static void main(String[] args){
+        Game game = new MovingRectangleGame();
+
+        game.start();
+    }
+}
---
+++ src/MovingRectangle/MovingRectangleGame.java
@@ -0,0 +1,22 @@
+package MovingRectangle;
+
+import Doctrina.Game;
+import Doctrina.Canvas;
+public class MovingRectangleGame extends Game{
+
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    protected void initialize() {
+
+    }
+}
